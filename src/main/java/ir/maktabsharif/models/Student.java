3c5f67d1e6eb8cf4ecfc1c83a6c89990b4165d6a
Nilu <nilu.shahmoradi@gmail.com>
2025-12-30 18:12:19 +0100

    feat(models): add Student and Teacher  entities

--- src/main/java/ir/maktabsharif/models/Student.java
+++ src/main/java/ir/maktabsharif/models/Student.java
@@ -1,4 +1,32 @@
 package ir.maktabsharif.models;
 
-public class Student {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = Student.TABLE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Student extends Person{
+
+    public static final String TABLE_NAME = "students";
+    public static final String STUDENT_CODE_COLUMN = "student_code";
+    public static final String FIELD_OF_STUDY_COLUMN = "field_of_study";
+    public static final String ENTRY_YEAR_COLUMN = "entry_year";
+
+    @Column(name = STUDENT_CODE_COLUMN)
+    private String studentCode;
+
+    @Column(name = FIELD_OF_STUDY_COLUMN)
+    private String fieldOfStudy;
+
+    @Column(name = ENTRY_YEAR_COLUMN)
+    private int entryYear;
 }
--- src/main/java/ir/maktabsharif/models/Teacher.java
+++ src/main/java/ir/maktabsharif/models/Teacher.java
@@ -1,4 +1,35 @@
 package ir.maktabsharif.models;
 
-public class Teacher {
+import ir.maktabsharif.models.enums.Degree;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = Teacher.TABLE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Teacher extends Person{
+
+    public static final String TABLE_NAME = "teachers";
+    public static final String TEACHER_CODE_COLUMN = "teacher_code";
+    public static final String QUALIFICATION_COLUMN = "qualification";
+    public static final String DEGREE_COLUMN = "degree";
+    public static final String MONTHLY_SALARY_COLUMN = "monthly_salary";
+
+    @Column(name = TEACHER_CODE_COLUMN)
+    private String teacherCode;
+
+    @Column(name = QUALIFICATION_COLUMN)
+    private String qualification;
+
+    @Column(name = DEGREE_COLUMN)
+    private Degree degree;
+
+    @Column(name = MONTHLY_SALARY_COLUMN)
+    private double monthlySalary;
 }
